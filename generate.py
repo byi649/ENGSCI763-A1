31d9948742be97f80143f1a2d6369be89151dd27
byi649 <byi649@aucklanduni.ac.nz>
2018-09-10 03:17:36 +1200

    Report

--- generate.py
+++ generate.py
@@ -8,7 +8,6 @@ for i in range(1000):
     random_set = [random.random() for x in range(10)]
     random_set.sort()
 
-    orders = random_set[2], random_set[4], random_set[6]
     average.append((random_set[2] + random_set[4] + random_set[6]) / 3.0)
 
 np.savetxt("list.csv", average)
